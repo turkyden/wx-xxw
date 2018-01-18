e4fd6311ccdf334525c3cad0c8500d286c2e2c37
邓大嘴 <616287091@qq.com>
2018-01-18 09:04:45 +0800

    update nodejs
    update nodejs

---
+++ app/controller/posts.js
@@ -0,0 +1,18 @@
+exports.index = async () => {
+    this.ctx.body = {
+        userId: `hello i am a Get`,
+        age: 56,
+    };
+};
+
+exports.new = async () => { };
+
+exports.create = async () => { };
+
+exports.show = async () => { };
+
+exports.edit = async () => { };
+
+exports.update = async () => { };
+
+exports.destroy = async () => { };
--- app/router.js
+++ app/router.js
@@ -6,4 +6,7 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+  router.get('/user/:id', controller.user.info);
+  router.resources('posts', '/api/posts', controller.posts);
+  //router.resources('users', '/api/v1/users', controller.v1.users);
 };
