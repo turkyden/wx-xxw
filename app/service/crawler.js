d3e55e534e31ef43f2add894413a973b25f097e8
邓大嘴 <616287091@qq.com>
2018-01-19 18:55:33 +0800

    update crawler
    update crawler

--- app/service/crawler.js
+++ app/service/crawler.js
@@ -10,24 +10,25 @@ class CrawlerService extends Service {
     }
     
     async getHTML (url) {
-        await http.get(url, (res) => {
+        var array = [];
+        http.get(url, function(res) {
             let html = '';
             res.setEncoding('utf-8');
-            res.on('data', (data) => {
+            res.on('data', function(data) {
                 html += data;
             })
-            res.on('end', (err) => {
+            res.on('end', function(err) {
                 if(err) {
                     console.log(err)
                 }else{
                     var $ = cheerio.load(html);
-                    let array = [];
+                    
                     const head = ['ferry_id', 'ferry_name', 'ferry_startTime', 'ferry_state',
                         'ferry_salabilityPassenger', 'ferry_salabilityCar', 'ferry_salabilityCoach', 'ferry_price'];
-                    $('#2integral_div1 table tr').each(function (i) {
+                    $('#2integral_div2 table tr').each(function (i) {
                         let json = {};
                         $(this).find('td').each(function (i) {
-                            json[head[i]] = $(this).html();
+                            json[head[i]] = $(this).text();
                             json['ferry_arriveTime'] = '01-18 21:00';
                             json['ferry_startPort'] = '海安港';
                             json['ferry_arrivePort'] = '海口秀英港';
@@ -36,17 +37,19 @@ class CrawlerService extends Service {
                             array.push(json);
                         }
                     })
-                    console.log(array);
-                    //return array;
+                    //console.log(array);
                 }
             })
         })
+        return array;
     }
 
     async getFerryService () {
         let r = await this.getHTML("http://www.haianport.com/Cursailok.php");
         //to do 保存航班数据
-        return 'hi'
+        console.log("获取服务");
+        console.log(r);
+        return r;
     }
 }
 
